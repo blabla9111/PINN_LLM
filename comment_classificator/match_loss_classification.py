7604d79bfcbc9d0d3df9e58c066a0de50983d5b6
blabla9111 <dinaragindullinaa@gmail.com>
2025-09-04 17:03:45 +0300

    weights_only=False

--- comment_classificator/match_loss_classification.py
+++ comment_classificator/match_loss_classification.py
@@ -76,7 +76,7 @@ def predict_text(model, tokenizer, text, device=None, top_k=2):
 def load_model():
     model = BertClassifier()
     state_dict = torch.load(
-    './comment_classificator/saved_models/model_weights_MATCH_LOSS_2_82_0.pth', weights_only=True)  # путь относительно корневой папки
+    './comment_classificator/saved_models/model_weights_MATCH_LOSS_2_82_0.pth', weights_only=False)  # путь относительно корневой папки
     model.load_state_dict(state_dict)
 
     return model
