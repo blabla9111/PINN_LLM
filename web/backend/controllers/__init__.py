c3667449558c87048a6456a6151ff390ccda3b38
blabla9111 <gindullina_2002@mail.ru>
2025-11-02 18:55:27 +0300

    added Mark's code

--- web/backend/controllers/__init__.py
+++ web/backend/controllers/__init__.py
@@ -2,10 +2,12 @@ from .model_generation_controller import ModelGenerationController
 from .training_controller import TrainingController
 from .analysis_controller import AnalysisController
 from .results_controller import ResultsController
+from .comment_validation_controller import ValidationController
 
 __all__ = [
     'ModelGenerationController',
     'TrainingController', 
     'AnalysisController',
-    'ResultsController'
+    'ResultsController',
+    'ValidationController'
 ]
